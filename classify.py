90674a12ec27c81b400d321244f707b68bc923ce
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2020-10-31 21:58:33 +0000

    might classify well

--- classify.py
+++ classify.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+'''
+Reference:
+    https://www.tensorflow.org/lite/guide/inference
+'''
+
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -10,7 +15,7 @@ import numpy
 import string
 import random
 import argparse
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import time
 
 def decode(characters, y):
@@ -18,13 +23,13 @@ def decode(characters, y):
     return ''.join([characters[x] for x in y])
 
 def preprocess(raw_data):
-    #rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
     img_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2GRAY)
     image = numpy.array(img_data) / 255.0
-    (c, h, w) = image.shape
-    image = image.reshape([-1, c, h, w])
+    (c, h) = image.shape
+    channels = 1
+    image = image.reshape([-1, c, h, channels])
+    image = image.astype('float32') 
     return image
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -32,6 +37,7 @@ def main():
     parser.add_argument('--captcha-dir', help='Where to read the captchas to break', type=str)
     parser.add_argument('--output', help='File where the classifications should be saved', type=str)
     parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
+    parser.add_argument('--captcha-len', help='Number of symbols (max) per captcha', type=int)
     args = parser.parse_args()
 
     if args.model_name is None:
@@ -50,6 +56,10 @@ def main():
         print("Please specify the captcha symbols file")
         exit(1)
 
+    if args.captcha_len is None:
+        print("Please specify the captcha length")
+        exit(1)
+
     symbols_file = open(args.symbols, 'r')
     captcha_symbols = symbols_file.readline().strip()
     symbols_file.close()
@@ -59,20 +69,32 @@ def main():
     start = time.time()
 
     #Load model
-    interpreter = tf.lite.Interpreter(args.model_name+'.tflite')
+    interpreter = tflite.Interpreter(args.model_name+'.tflite')
     interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
 
     with open(args.output, 'w') as output_file:
         for x in os.listdir(args.captcha_dir):
             # load image and process it
             raw_data = cv2.imread(os.path.join(args.captcha_dir, x))
             image = preprocess(raw_data)
+            interpreter.set_tensor(input_details[0]['index'], image)
+            interpreter.invoke()
             
-            
-            prediction = model.predict(image)
+            prediction = ''
+            for i in range(args.captcha_len):
+                output_data = interpreter.get_tensor(output_details[i]['index'])[0]
+                max_val = 0
+                idx = 0
+                for i in range(len(output_data)):
+                    if output_data[i] >= max_val:
+                        max_val = output_data[i]
+                        idx = i
+                prediction += captcha_symbols[idx]
+            prediction.replace(' ', '')
             output_file.write(x + "," + decode(captcha_symbols, prediction) + "\n")
 
-            print('Classified ' + x)
     end = time.time()
     print('Time: ' + str(end - start))
 
@@ -80,5 +102,5 @@ if __name__ == '__main__':
     main()
 
 '''
-python classify.py --model-name model/model_1 --captcha-dir in/pre/ --output out/model_1_output.txt --symbols model/symbols.txt
+python classify.py --model-name model/model_1 --captcha-dir in/temp/ --output out/model_1_output.txt --symbols model/symbols.txt --captcha-len 5
 '''
