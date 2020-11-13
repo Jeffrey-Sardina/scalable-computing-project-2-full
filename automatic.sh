32036e0b912ea593460340e04f092c69b39909e0
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2020-11-13 11:57:57 +0000

    re-org

--- automatic.sh
+++ automatic.sh
@@ -12,24 +12,22 @@ then
     rm -r model/est/
     rm -r model/val/
     rm -r model/gen/
-
     mkdir model/est/
     mkdir model/val/
     mkdir model/gen/
-
-    powershell.exe -File ./generate.ps1
+    ./generate.sh
 else
     echo $exitstatus 'Using existing captchas'
 fi
 
-#Create model
-powershell.exe -File ./train.ps1
+# #Create model
+# powershell.exe -File train.ps1
 
-#Copy model to pi repo
-cp model/model.tflite ../Tionscadal\ 2\ pi/model/model.tflite
+# #Copy model to pi repo
+# cp model/model.tflite ../Tionscadal\ 2\ pi/model/model.tflite
 
-#Push to git
-cd ../Tionscadal\ 2\ pi/model/model.tflite
-git add *
-git commit -m "new model"
-git push
+# #Push to git
+# cd ../Tionscadal\ 2\ pi/model/model.tflite
+# git add *
+# git commit -m "new model"
+# git push
