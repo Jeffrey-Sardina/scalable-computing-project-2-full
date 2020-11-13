c260e283d29cc676d2a153d7307bccddbf79c075
Jeffrey <jeffrey.sardina@gmail.com>
2020-11-13 15:06:22 +0000

    new model

--- automatic.sh
+++ automatic.sh
@@ -1,33 +1,33 @@
-#!/bin/bash
+# #!/bin/bash
 
-if [[ "$#" -lt 1 ]]
-then
-    echo 'Please provide the following arguments: whether to re-create the training set (0 or 1)'
-    exit 1
-fi
+# if [[ "$#" -lt 1 ]]
+# then
+#     echo 'Please provide the following arguments: whether to re-create the training set (0 or 1)'
+#     exit 1
+# fi
 
-if [[ $1 -eq 1 ]]
-then
-    echo $exitstatus 'Deleting existing captchas and generating new ones'
-    rm -r model/est/
-    rm -r model/val/
-    rm -r model/gen/
-    mkdir model/est/
-    mkdir model/val/
-    mkdir model/gen/
-    ./generate.sh
-else
-    echo $exitstatus 'Using existing captchas'
-fi
+# if [[ $1 -eq 1 ]]
+# then
+#     echo $exitstatus 'Deleting existing captchas and generating new ones'
+#     rm -r model/est/
+#     rm -r model/val/
+#     rm -r model/gen/
+#     mkdir model/est/
+#     mkdir model/val/
+#     mkdir model/gen/
+#     ./generate.sh
+# else
+#     echo $exitstatus 'Using existing captchas'
+# fi
 
 # #Create model
 # powershell.exe -File train.ps1
 
-# #Copy model to pi repo
-# cp model/model.tflite ../Tionscadal\ 2\ pi/model/model.tflite
+#Copy model to pi repo
+cp model/model.tflite ../Tionscadal\ 2\ pi/model/model.tflite
 
-# #Push to git
-# cd ../Tionscadal\ 2\ pi/model/model.tflite
-# git add *
-# git commit -m "new model"
-# git push
+#Push to git
+cd ../Tionscadal\ 2\ pi/model/model.tflite
+git add *
+git commit -m "new model"
+git push
--- train.ps1
+++ train.ps1
@@ -1,1 +1,3 @@
-python3 train.py --width 128 --height 64 --length 6 --symbols model/symbols.txt --batch-size 32 --epochs 100 --output-model-name model/model --train-dataset model/gen/ --validate-dataset model/val/
+echo 'starting trainimg'
+python train.py --width 128 --height 64 --length 6 --symbols model/symbols.txt --batch-size 32 --epochs 100 --output-model-name model/model --train-dataset model/gen/ --validate-dataset model/val/
+echo 'ended trainimg'
