0171cb9296bbe319d27a7aa91c1df0db757fb958
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2020-11-15 15:54:09 +0000

    Model 15 in

--- generate.sh
+++ generate.sh
@@ -6,6 +6,6 @@ python3 generate.py --width 128 --height 64 --length 6 --symbols model/symbols_g
 python3 generate.py --width 128 --height 64 --length 6 --symbols model/symbols_gen.txt --count $((((100 - $4) * $3) / 100)) --output-dir model/est/ --processes 8
 
 #monochar
-python3 generate.py --width 128 --height 64 --length 6 --symbols model/symbols_gen.txt --count $((($4 * $1) / 100)) --output-dir model/gen/ --processes 8 --mono-char 1 
-python3 generate.py --width 128 --height 64 --length 6 --symbols model/symbols_gen.txt --count $((($4 * $2) / 100)) --output-dir model/val/ --processes 8 --mono-char 1 
-python3 generate.py --width 128 --height 64 --length 6 --symbols model/symbols_gen.txt --count $((($4 * $3) / 100)) --output-dir model/est/ --processes 8 --mono-char 1 
+python3 generate.py --width 128 --height 64 --length 6 --symbols model/symbols_gen.txt --count $1 --output-dir model/gen/ --processes 8 --mono-char 1 
+python3 generate.py --width 128 --height 64 --length 6 --symbols model/symbols_gen.txt --count $2 --output-dir model/val/ --processes 8 --mono-char 1 
+python3 generate.py --width 128 --height 64 --length 6 --symbols model/symbols_gen.txt --count $3 --output-dir model/est/ --processes 8 --mono-char 1 
